fd3b33af446241bd8f4813de604aa71d2b2de45f
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-18 16:25:04 +0400

    x

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,6 +118,31 @@
       },
     },
   });
+  new Swiper(".carousel-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-course-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
 
   /**
    * Animation on scroll
